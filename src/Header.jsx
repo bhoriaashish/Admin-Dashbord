232a501d29a073c4f24816034ce229d53553dc9f
bhoriaashish <Ashishbhoria12@gmail.com>
2023-10-17 16:37:53 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+//   BsFillBillFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from 'react-icons/bs';
+
+function Header() {
+  return (
+    <header>
+      <div className='menu-icon'>
+        <BsJustify className='icon' />
+      </div>
+      <div className='header-left'>
+        <BsSearch className='icon' />
+      </div>
+      <div className='header-right'>
+        {/* <BsFillBillFill className='icon' /> */}
+        <BsFillEnvelopeFill className='icon' />
+        <BsPersonCircle className='icon' />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
